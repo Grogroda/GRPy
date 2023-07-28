291ca7e1f7f2c54112fe00279008b37a5979e3ef
Arthur Diniz Meirelles <meirellesarthur@usp.br>
2023-07-28 11:08:42 -0300

    Basic tensor definition ready!

--- tensor.py
+++ tensor.py
@@ -1,14 +1,19 @@
 import numpy as np
-import simpy as sp
+import sympy as sp
 import copy
 
-def change_tensor(lista):
-    for i in range(len(lista)):
-        if type(lista[i])==list:
-            change_tensor(lista[i])
+def change_tensor(lista, inds, layer):
+    '''
+    This function enters the term lista[ins[0]][inds[1]]... and changes it to the term desired by the user.
+    '''
+    if type(lista[inds[layer]])==list:
+        lista[inds[layer]]=change_tensor(lista[inds[layer]], inds, layer+1)
 
-        else:
-            lista[i]=sp.sympify(input('Choose a component (in order): '))
+    else:
+        term=sp.sympify(input('\nChoose a number or function for the term {}: '.format(inds)))
+        lista[inds[layer]]=term
+
+    return lista
 
 def build_tensor(rank, nb_per_level, fill=0):
     '''
@@ -22,9 +27,10 @@ def build_tensor(rank, nb_per_level, fill=0):
 
 class Tensor:
 
-    def __init__(self, struct, lista=[], coords=sp.symbols('t x y z')):
+    def __init__(self, struct, non_zeros, lista=[], coords=sp.symbols('t x y z')):
         '''
        The user has to provide the structure of the tensor in the form of a list o 0s and 1s, 0 indicates a lower index and 1 indicates an upper index, indicating only the amount of coordinates of the tensor and the index structure. Optionally, they can pass a list of lists in a form suitable to the structure provided, which will be verified to make sure it matches the structure provided. 
+       There's kind of a but in the creation of the "lista", if the intended tensor is a vector, the list non_zeros needs to have the format [[i],[j], ...], it would be ideal if the used could pass (for instance) [i,j,...] also. 
         '''
 
         self.struct=struct #list of 0s and 1s
@@ -33,12 +39,27 @@ class Tensor:
         if lista!=[]:
             self.lista=lista
             #test if the list corresponds to the structure given
+            #if it doesn't, set the list to list=[], so the next condition works
+            #pass an exception error if the format doesn't correspond to the given structure
             
         if lista==[]: 
-            print("Pass the components of your tensor:")
-            self.lista=build_tensor(self.rank, 4)
-            change_tensor(self.list)
+            lista=build_tensor(self.rank-1, 4)
+            print("lista=", lista, '\n')
+            print("Pass the non-zero components of your tensor:\n")
+            for inds in non_zeros:
+                self.lista=change_tensor(lista, inds, 0)
 
-        #The tensor creation is crappy but works. Fix later:
-        #The user manually chooses non-zero components > manually choosing all components
+        #Extensively test the tensor creation to see if it works as intended
         #Use something other than deepcopy for the initial tensor creation
+
+
+
+if __name__=="__main__":
+    '''
+    This is a testing area, every bit of code written here will only be run if you directly run the tensor.py code.
+    '''
+
+    test=Tensor([0,0], [[0,0], [1,1], [2,2], [3,3]])
+    print("Here is your test tensor:\n")
+    tensor=np.array(test.lista)
+    print(tensor)
