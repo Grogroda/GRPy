0ed7dd75a044fd14641674f18982f10d107776ad
Arthur Diniz Meirelles <meirellesarthur@usp.br>
2023-07-28 17:15:38 -0300

    Definition of tensor equality, sum and scalar multiplication pending. .gitignore created.

--- tensor.py
+++ tensor.py
@@ -27,7 +27,7 @@ def build_tensor(rank, nb_per_level, fill=0):
 
 class Tensor:
 
-    def __init__(self, struct, non_zeros, lista=[], coords=sp.symbols('t x y z')):
+    def __init__(self, struct, non_zeros, lista=[], metric=None, coords=sp.symbols('t x y z')):
         '''
        The user has to provide the structure of the tensor in the form of a list o 0s and 1s, 0 indicates a lower index and 1 indicates an upper index, indicating only the amount of coordinates of the tensor and the index structure. Optionally, they can pass a list of lists in a form suitable to the structure provided, which will be verified to make sure it matches the structure provided. 
        There's kind of a but in the creation of the "lista", if the intended tensor is a vector, the list non_zeros needs to have the format [[i],[j], ...], it would be ideal if the used could pass (for instance) [i,j,...] also. 
@@ -35,6 +35,8 @@ class Tensor:
 
         self.struct=struct #list of 0s and 1s
         self.rank=len(struct)
+        self.metric=metric
+        self.coords=coords
 
         if lista!=[]:
             self.lista=lista
@@ -51,15 +53,34 @@ class Tensor:
 
         #Extensively test the tensor creation to see if it works as intended
         #Use something other than deepcopy for the initial tensor creation
+        self.lista=np.array(self.lista)
 
+        def __eq__(T1, T2):
+            if T1.struct==T2.struct:
+                #Not working in any way whatsoever
+                return True
+            else:
+                return False
 
+        def __add__(self, T):
+            #check if the structure is compatible, then add with numpy
+            
+            return None
+
+        def __prod__(self, k):
+            #if k is a number, product with scalar; if k is another tensor -> too much work for now
+            if type(k)==float or type(k)==int:
+                #prod por escalar
+                return None
+            else:
+                #contração
+                return None
+
+g_mink=Tensor([0,0], [], [[-1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]], metric=[])
 
 if __name__=="__main__":
     '''
     This is a testing area, every bit of code written here will only be run if you directly run the tensor.py code.
     '''
+    
 
-    test=Tensor([0,0], [[0,0], [1,1], [2,2], [3,3]])
-    print("Here is your test tensor:\n")
-    tensor=np.array(test.lista)
-    print(tensor)
