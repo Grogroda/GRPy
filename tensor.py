099ec3662ca9cf1c683a7d7fecb07e1b96702a3f
Arthur Diniz Meirelles <meirellesarthur@usp.br>
2023-08-04 14:00:31 -0300

    __str__ and __repr__ methods added to tensor.py

--- tensor.py
+++ tensor.py
@@ -2,18 +2,18 @@ import numpy as np
 import sympy as sp
 import copy
 
-def change_tensor(lista, inds, layer):
+def change_tensor(arr, inds, layer):
     '''
-    This function enters the term lista[ins[0]][inds[1]]... and changes it to the term desired by the user.
+    This function enters the term arr[ins[0]][inds[1]]... and changes it to the term desired by the user.
     '''
-    if type(lista[inds[layer]])==list:
-        lista[inds[layer]]=change_tensor(lista[inds[layer]], inds, layer+1)
+    if type(arr[inds[layer]])==list:
+        arr[inds[layer]]=change_tensor(arr[inds[layer]], inds, layer+1)
 
     else:
         term=sp.sympify(input('\nChoose a number or function for the term {}: '.format(inds)))
-        lista[inds[layer]]=term
+        arr[inds[layer]]=term
 
-    return lista
+    return arr
 
 def build_tensor(rank, nb_per_level, fill=0):
     '''
@@ -27,10 +27,10 @@ def build_tensor(rank, nb_per_level, fill=0):
 
 class Tensor:
 
-    def __init__(self, struct, non_zeros, lista=[], metric=None, coords=sp.symbols('t x y z')):
+    def __init__(self, struct, non_zeros, arr=[], metric=None, coords=sp.symbols('t x y z')):
         '''
        The user has to provide the structure of the tensor in the form of a list o 0s and 1s, 0 indicates a lower index and 1 indicates an upper index, indicating only the amount of coordinates of the tensor and the index structure. Optionally, they can pass a list of lists in a form suitable to the structure provided, which will be verified to make sure it matches the structure provided. 
-       There's kind of a but in the creation of the "lista", if the intended tensor is a vector, the list non_zeros needs to have the format [[i],[j], ...], it would be ideal if the used could pass (for instance) [i,j,...] also. 
+       There's kind of a but in the creation of the "arr", if the intended tensor is a vector, the list non_zeros needs to have the format [[i],[j], ...], it would be ideal if the used could pass (for instance) [i,j,...] also. 
         '''
 
         self.struct=struct #list of 0s and 1s
@@ -38,37 +38,37 @@ class Tensor:
         self.metric=metric
         self.coords=coords
 
-        if lista!=[]:
-            self.lista=lista
+        if arr!=[]:
+            self.arr=arr
             #test if the list corresponds to the structure given
             #if it doesn't, set the list to list=[], so the next condition works
             #pass an exception error if the format doesn't correspond to the given structure
             
-        if lista==[]: 
-            lista=build_tensor(self.rank-1, 4)
-            print("lista=", lista, '\n')
+        if arr==[]: 
+            arr=build_tensor(self.rank-1, 4)
+            print("arr=", arr, '\n')
             print("Pass the non-zero components of your tensor:\n")
             for inds in non_zeros:
-                self.lista=change_tensor(lista, inds, 0)
+                self.arr=change_tensor(arr, inds, 0)
 
         #Extensively test the tensor creation to see if it works as intended
         #Use something other than deepcopy for the initial tensor creation
-        self.lista=np.array(self.lista)
+        self.arr=np.array(self.arr)
 
     def __eq__(T1, T2):
-        if T1.struct==T2.struct and np.array_equal(T1.lista,T2.lista) and T1.coords==T2.coords: #How to verify if the metric is equal?
+        if T1.struct==T2.struct and np.array_equal(T1.arr,T2.arr) and T1.coords==T2.coords: #How to verify if the metric is equal?
             return True
         else:
             return False
 
     def __add__(self, T):
-        npself=np.array(self.lista)
-        npT=np.array(T.lista)
+        npself=np.array(self.arr)
+        npT=np.array(T.arr)
 
         if self.struct==T.struct and self.struct==T.struct and self.coords==T.coords:
             arr_sum=npT+npself
             list_sum=arr_sum.tolist()
-            return Tensor(self.struct, [], lista=list_sum, metric=self.metric, coords=self.coords) 
+            return Tensor(self.struct, [], arr=list_sum, metric=self.metric, coords=self.coords) 
 
         else:
             raise Exception("Tensors are somehow incompatible!")
@@ -77,9 +77,9 @@ class Tensor:
     def __mul__(self, k):
         #if k is a number, product with scalar; if k is another tensor -> too much work for now
         if type(k)==float or type(k)==int:
-            arr_prod=k*self.lista
+            arr_prod=k*self.arr
             list_prod=arr_prod.tolist()
-            return Tensor(self.struct, [], lista=list_prod, metric=self.metric, coords=self.coords) 
+            return Tensor(self.struct, [], arr=list_prod, metric=self.metric, coords=self.coords) 
         else:
             print("Tensor contraction not yet implemented")
             #contraction
@@ -87,6 +87,12 @@ class Tensor:
 
     def __rmul__(self,k):
         return self.__mul__(k)
+
+    def __str__(self):
+        return str(self.arr)
+
+    def __repr__(self):
+        return str(self.arr)
 
 g_mink=Tensor([0,0], [], [[-1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]], metric=[])
 
